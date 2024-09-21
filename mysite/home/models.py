9d697dab5eb21253cf1035e9e63fe16620debdc8
goyal160 <ayush.intense@gmail.com>
2024-09-22 01:32:41 +0530

    updated files

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('BS', 'Barik Sev'),
+    ('MS', 'Moti Sev'),
+    ('CD', 'Chana Dal'),
+    ('CB', 'Chatpati Boondi'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    sp = models.FloatField()
+    dp = models.FloatField()
+    desc = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.db.models import Count
+from .models import Product
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home/home.html')
+
+class CategoryView(View):
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, 'home/category.html', locals())
+
+class ProductDetail(View):
+    def get(self, request, pk):
+        return render(request, 'home/productdetail.html', locals())
