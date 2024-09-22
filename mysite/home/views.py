038a127a3514b6334d1f34d7ad570fc2adbb8f94
goyal160 <ayush.intense@gmail.com>
2024-09-23 00:14:17 +0530

    login updated

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.db.models import Count
 from .models import Product
+from . forms import CustomerRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -30,3 +32,17 @@ class ProductDetail(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'home/productdetail.html', locals())
+    
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'home/customer_registration.html', locals())
+    
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Welcome on board!")
+        else:
+            messages.warning(request, "Invalid data entry")
+        return render(request, 'home/customer_registration.html', locals())
