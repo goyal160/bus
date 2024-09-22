82ee7de6d6ccb330e9c906d4b09f8c3f15104075
goyal160 <ayush.intense@gmail.com>
2024-09-22 16:18:56 +0530

    updated images

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -8,12 +8,25 @@ from .models import Product
 def home(request):
     return render(request, 'home/home.html')
 
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
 class CategoryView(View):
     def get(self, request, val):
         product = Product.objects.filter(category=val)
         title = Product.objects.filter(category=val).values('title')
         return render(request, 'home/category.html', locals())
 
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, 'home/category.html', locals())
+
 class ProductDetail(View):
     def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
         return render(request, 'home/productdetail.html', locals())
