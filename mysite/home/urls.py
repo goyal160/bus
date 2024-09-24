d73428333edde6bb1c7a2071d1873e6b96c439a1
goyal160 <ayush.intense@gmail.com>
2024-09-24 23:20:10 +0530

    updated till logout

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,7 +3,7 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordResetForm
+from .forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
 
 urlpatterns = [
     path('', views.home),
@@ -13,11 +13,15 @@ urlpatterns = [
     path('category-title/<val>', views.CategoryTitle.as_view(), name='category-title'),
     path('product-detail/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('address/', views.ProfileView.as_view(), name='address'),
+    path('address/', views.address, name='address'),
+    path('update_address/<int:pk>', views.UpdateAddress.as_view(), name='update_address'),
 
     #login authentication
     path('registration/', views.CustomerRegistrationView.as_view(), name='customer_registration'),
     path('accounts/login/', auth_view.LoginView.as_view(template_name='home/login.html', authentication_form=LoginForm), name='login'),
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name='home/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='home/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='home/passwordchangedone.html'), name='passwordchangedone'),
+    path('logout/', auth_view.LogoutView.as_view(next_page='login'), name='logout'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.db.models import Count
 from .models import Product, Customer
@@ -72,3 +72,24 @@ class ProfileView(View):
 def address(request):
     add = Customer.objects.filter(user=request.user)
     return render(request, 'home/address.html', locals())
+
+class UpdateAddress(View):
+    def get(self, request, pk):
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request, 'home/update_address.html', locals())
+    def post(self, request, pk):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request, "Address updated successfully.")
+        else:
+            messages.warning(request, "Invalid data entry. Please try again.")
+        return redirect("address")
